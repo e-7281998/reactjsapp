0fd8b7e6ed8e5ca78a2376b39b73a41cd55e3aba
e-7281998 <jejjcs43@naver.com>
2023-05-25 14:49:36 +0900

    [context API] 설명 추가

--- src/components4/CounterProvider.js
+++ src/components4/CounterProvider.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { createContext } from "react";
 
+//createContext로 공유영역 만듬
 const CounterContext = createContext();
 
 const CounterProvider = (props) => {
@@ -19,6 +20,8 @@ const CounterProvider = (props) => {
 
   //변수명과 값이 같으면 굳이 2번 작성하지 않아도 됨
   //Provider : 제공하고 싶은 것.
+  //CounterProvider의 자식들은 value에 등록된 값들을 공유해서 사용하게 됨(global)
+  //자식들은 모든 값을 다 상속{ } 받지 않아도 됨. 필요한 값들만 골라서 사용할 수 있음
   return (
     <div>
       <CounterContext.Provider
--- src/components4/ProviderTest.js
+++ src/components4/ProviderTest.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useContext } from "react";
+//createContext를 상속받은 컴포넌트임..
 import { CounterContext } from "./CounterProvider";
 
 const CountLabel = () => {
+  //Provider된 값들 중 필요한 값만 골라서 사용하기
+  //username의 경우 PlusButton에서 변경해도 CountLabel의 값이 같이 변경됨. (Global영역이므로 - contextAPI)
   const { count, username, mystyle } = useContext(CounterContext);
 
   return (
